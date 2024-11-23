ac90979a9e24cfdd50437b116900ab74cecab9a4
Vincent Duchauffour <vincent.duchauffour@proton.me>
2024-11-23 23:52:06 +0100

    Add tests to verify the event processing

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,3 +1,5 @@
+import pytest
+
 from fastgithub.handler import GithubWebhookHandler
 from fastgithub.signature import SignatureVerificationSHA256
 
@@ -37,3 +39,19 @@ def test_function_is_append_using_list():
 
     assert webhook_handler.webhooks["push"] == [foo, bar]
     assert webhook_handler.webhooks == webhook_handler._webhooks
+
+
+@pytest.mark.asyncio
+async def test_process_event():
+    webhook_handler = GithubWebhookHandler(signature_verification=None)
+
+    def foo(*args, **kwargs):
+        pass
+
+    def bar(*args, **kwargs):
+        raise
+
+    webhook_handler.listen("push", [foo])
+    assert await webhook_handler.process_event("push", {}) is True
+    webhook_handler.listen("pull_request", [bar])
+    assert await webhook_handler.process_event("pull_request", {}) is False

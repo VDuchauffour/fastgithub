e24530a2684e5668ad2e348244b75124aa0edccf
Vincent Duchauffour <vincent.duchauffour@proton.me>
2024-11-24 02:31:22 +0100

    Reorder modules

--- src/fastgithub/__init__.py
+++ src/fastgithub/__init__.py
@@ -3,7 +3,7 @@
 from importlib.metadata import version
 
 from .endpoint.webhook_router import webhook_router
-from .handler import GithubWebhookHandler
-from .signature import SignatureVerificationSHA1, SignatureVerificationSHA256
+from .webhook.handler import GithubWebhookHandler
+from .webhook.signature import SignatureVerificationSHA256
 
 __version__ = version("fastgithub")

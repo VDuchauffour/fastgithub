712fcefe06f56b6716262c177a16220ff1352e34
Vincent Duchauffour <40724343+VDuchauffour@users.noreply.github.com>
2024-11-27 12:02:28 +0100

    Use dynamic vcs version (#22)

--- src/fastgithub/__init__.py
+++ src/fastgithub/__init__.py
@@ -1,10 +1,7 @@
 """FastGitHub."""
 
-from importlib.metadata import version
-
+from ._version import __version__, __version_tuple__
 from .endpoint.webhook_router import webhook_router
 from .recipes import GithubRecipe, Recipe
 from .webhook.handler import GithubWebhookHandler
 from .webhook.signature import SignatureVerificationSHA256
-
-__version__ = version("fastgithub")

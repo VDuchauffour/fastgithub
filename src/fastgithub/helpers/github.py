364a921fd7cce9ad75a3ff3c8c956dcb8bb3d780
Vincent Duchauffour <vincent.duchauffour@proton.me>
2025-01-22 14:53:48 +0100

    Enhance label definitions

--- src/fastgithub/helpers/github.py
+++ src/fastgithub/helpers/github.py
@@ -1,6 +1,17 @@
+import github
+import github.Label
 from github import Github, RateLimitExceededException
 from github.PullRequest import PullRequest
 from github.RateLimit import RateLimit
+from pydantic import BaseModel
+
+
+class Label(BaseModel):
+    """A data model for GitHub label."""
+
+    name: str
+    color: str
+    description: str
 
 
 class RateStatus:
@@ -35,12 +46,8 @@ class RateStatus:
 class GithubHelper:
     def __init__(self, github: Github, repo_fullname: str, rate_threshold: float = 0.5) -> None:
         self._github = github
-        self._rate_status = RateStatus(self.github, rate_threshold)
+        self._rate_status = RateStatus(github, rate_threshold)
         self.repo = github.get_repo(repo_fullname, lazy=True)
-
-    @property
-    def github(self) -> Github:
-        return self._github
 
     @property
     def rate_status(self) -> RateStatus:
@@ -48,22 +55,44 @@ class GithubHelper:
 
     def raise_for_rate_excess(self) -> None:
         if self.rate_status.too_low():
-            status = self.rate_status.status
-            assert isinstance(status, RateLimit)  # noqa: S101
+            status: RateLimit = self.rate_status.status  # type: ignore
             raise RateLimitExceededException(
                 429,
                 status.core.raw_data,
                 status.core.raw_headers,  # type: ignore
             )
 
-    @staticmethod
-    def extract_labels_from_pr(pr: PullRequest, labels_config: dict[str, list[str]]) -> set[str]:
+    def _get_or_create_label(
+        self,
+        name: str,
+        color: str = "ff66cc",
+        description: str = "Created by FastGitHub",
+    ) -> github.Label.Label:
+        """Fetch an existing label or create it."""
+        try:
+            label = self.repo.get_label(name)
+        except github.UnknownObjectException:
+            label = None
+
+        if not label:
+            label = self.repo.create_label(
+                name=name,
+                color=color,
+                description=description,
+            )
+        return label
+
+    def extract_labels_from_pr(
+        self, pr: PullRequest, labels_config: dict[str, list[Label]]
+    ) -> set[str]:
         labels = set()
         commit_messages = [c.commit.message for c in pr.get_commits()]
         for message in commit_messages:
             for pattern, labels_ in labels_config.items():
                 if pattern in message:
-                    labels.update(labels_)
+                    for label_ in labels_:
+                        label_ = self._get_or_create_label(**label_.model_dump())
+                        labels.update(label_.name)
         return labels
 
     @staticmethod

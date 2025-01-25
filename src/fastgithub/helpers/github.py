091c6544701df2674407c9e5239e93b6c4f55adc
Vincent Duchauffour <40724343+VDuchauffour@users.noreply.github.com>
2025-01-25 17:33:40 +0100

    Enhance label definitions (#28)

--- src/fastgithub/helpers/github.py
+++ src/fastgithub/helpers/github.py
@@ -1,6 +1,19 @@
+import re
+
+import github
+import github.Label
 from github import Github, RateLimitExceededException
 from github.PullRequest import PullRequest
 from github.RateLimit import RateLimit
+from pydantic import BaseModel
+
+
+class Label(BaseModel):
+    """A data model for GitHub label."""
+
+    name: str
+    color: str
+    description: str
 
 
 class RateStatus:
@@ -33,14 +46,12 @@ class RateStatus:
 
 
 class GithubHelper:
+    LABEL_REGEX = re.compile(r"#([a-z][a-z1-9-]+)([^a-z1-9-]|$)")
+
     def __init__(self, github: Github, repo_fullname: str, rate_threshold: float = 0.5) -> None:
         self._github = github
-        self._rate_status = RateStatus(self.github, rate_threshold)
+        self._rate_status = RateStatus(github, rate_threshold)
         self.repo = github.get_repo(repo_fullname, lazy=True)
-
-    @property
-    def github(self) -> Github:
-        return self._github
 
     @property
     def rate_status(self) -> RateStatus:
@@ -48,22 +59,62 @@ class GithubHelper:
 
     def raise_for_rate_excess(self) -> None:
         if self.rate_status.too_low():
-            status = self.rate_status.status
-            assert isinstance(status, RateLimit)  # noqa: S101
+            status: RateLimit = self.rate_status.status  # type: ignore
             raise RateLimitExceededException(
                 429,
                 status.core.raw_data,
                 status.core.raw_headers,  # type: ignore
             )
 
+    def _get_or_create_label(
+        self,
+        name: str,
+        color: str = "ff66cc",
+        description: str = "Created by FastGitHub",
+    ) -> github.Label.Label:
+        """Fetch an existing label or create it."""
+        try:
+            label = self.repo.get_label(name)
+        except github.UnknownObjectException:
+            label = None
+
+        if not label:
+            label = self.repo.create_label(
+                name=name,
+                color=color,
+                description=description,
+            )
+        return label
+
     @staticmethod
-    def extract_labels_from_pr(pr: PullRequest, labels_config: dict[str, list[str]]) -> set[str]:
+    def validate_label_name(name: str, pattern: re.Pattern) -> None:
+        """Validate the name of a label given a regex pattern."""
+        if not pattern.fullmatch(name):
+            raise ValueError(
+                f"The pattern `{name}` don't follow the regex {GithubHelper.LABEL_REGEX.pattern}!"
+            )
+
+    def extract_labels_from_commit(
+        self, message: str, labels_config: dict[str, list[Label]]
+    ) -> set[str]:
+        """Extract labels from a commit message and create labels in the repo if needed."""
+        labels = set()
+        for pattern, labels_ in labels_config.items():
+            self.__class__.validate_label_name(pattern, GithubHelper.LABEL_REGEX)
+            if pattern in message:
+                for label_ in labels_:
+                    label_ = self._get_or_create_label(**label_.model_dump())
+                    labels.update([label_.name])
+        return labels
+
+    def extract_labels_from_pr(
+        self, pr: PullRequest, labels_config: dict[str, list[Label]]
+    ) -> set[str]:
+        """Extract labels from a PR and create labels in the repo if needed."""
         labels = set()
         commit_messages = [c.commit.message for c in pr.get_commits()]
         for message in commit_messages:
-            for pattern, labels_ in labels_config.items():
-                if pattern in message:
-                    labels.update(labels_)
+            labels = labels.union(self.extract_labels_from_commit(message, labels_config))
         return labels
 
     @staticmethod
--- src/fastgithub/recipes/github/labels_from_commits.py
+++ src/fastgithub/recipes/github/labels_from_commits.py
@@ -1,30 +1,29 @@
 from collections.abc import Callable
 
-from fastgithub.helpers.github import GithubHelper
+from github import Github
+
+from fastgithub.helpers.github import GithubHelper, Label
 from fastgithub.recipes._base import GithubRecipe
 from fastgithub.types import Payload
 
-LABEL_CONFIG: dict[str, list[str]] = {
-    "#nodraft": ["nodraft"],
-    "#fast": ["nodraft", "automerge", "autoapprove"],
-    "#release": ["nodraft", "automerge", "autorelease"],
-    "#furious": ["nodraft", "automerge", "autoapprove", "autorelease"],
-}
+from ._config import LABEL_CONFIG
 
 
 class LabelsFromCommits(GithubRecipe):
+    def __init__(
+        self, github: Github, labels_config: dict[str, list[Label]] = LABEL_CONFIG
+    ) -> None:
+        super().__init__(github)
+        self.labels_config = labels_config
+
     @property
     def events(self) -> dict[str, Callable]:
         return {"pull_request": self._process_push}
 
-    def _process_push(
-        self,
-        payload: Payload,
-        labels_config: dict[str, list[str]] = LABEL_CONFIG,
-    ):
+    def _process_push(self, payload: Payload):
         gh = GithubHelper(self.github, payload["repository"]["full_name"])
         gh.raise_for_rate_excess()
 
         pr = gh.repo.get_pull(payload["number"])
-        if labels := gh.extract_labels_from_pr(pr, labels_config):
+        if labels := gh.extract_labels_from_pr(pr, self.labels_config):
             gh.add_labels_to_pr(pr, labels)
